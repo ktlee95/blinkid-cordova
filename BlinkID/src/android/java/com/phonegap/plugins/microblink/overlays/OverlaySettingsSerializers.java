0c875ca4142077cb0e49fdbdb7cc1931c3732dc7
ktlee <ktlee@ktlees-MacBook-Pro.local>
2020-02-25 09:52:32 +0800

    v4.11.1

--- BlinkID/src/android/java/com/phonegap/plugins/microblink/overlays/OverlaySettingsSerializers.java
+++ BlinkID/src/android/java/com/phonegap/plugins/microblink/overlays/OverlaySettingsSerializers.java
@@ -21,8 +21,10 @@ public enum OverlaySettingsSerializers {
     }
 
     OverlaySettingsSerializers() {
+        registerMapping(new BarcodeOverlaySettingsSerialization());
         registerMapping(new DocumentOverlaySettingsSerialization());
         registerMapping(new DocumentVerificationOverlaySettingsSerialization());
+        registerMapping(new BlinkCardOverlaySettingsSerialization());
         registerMapping(new BlinkIdOverlaySettingsSerialization());
     }
 
